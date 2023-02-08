f483e91213f9cde6bdd1983929390746524eb70e
Michael Seaton <mseaton@pih.org>
2023-02-08 11:03:40 -0500

    Change to generic container (#2)

--- api/src/test/java/org/openmrs/module/dbevent/test/Mysql.java
+++ api/src/test/java/org/openmrs/module/dbevent/test/Mysql.java
@@ -3,10 +3,9 @@ package org.openmrs.module.dbevent.test;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.utility.DockerImageName;
-import org.testcontainers.utility.MountableFile;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.io.Closeable;
 import java.util.Properties;
@@ -18,23 +17,17 @@ public class Mysql implements Closeable {
 
     public static final String DATABASE_NAME = "dbevent";
 
-    private MySQLContainer<?> container;
+    private GenericContainer<?> container;
 
     private Mysql() {}
 
     public static Mysql open() throws Exception {
         Mysql mysql = new Mysql();
-        MySQLContainer<?> container = new MySQLContainer<>(DockerImageName.parse("mysql:5.6"))
-                .withCopyFileToContainer(
-                        MountableFile.forClasspathResource("mysql/my.cnf"),
-                        "/etc/mysql/my.cnf"
-                )
-                .withCopyFileToContainer(
-                        MountableFile.forClasspathResource("mysql/initial-25x.sql"),
-                        "/docker-entrypoint-initdb.d/setup.sql"
-                )
-                .withDatabaseName(DATABASE_NAME)
-                .withLogConsumer(new Slf4jLogConsumer(log));
+        GenericContainer<?> container = new GenericContainer<>(new ImageFromDockerfile()
+                        .withFileFromClasspath("Dockerfile", "mysql/Dockerfile")
+                        .withFileFromClasspath("my.cnf", "mysql/my.cnf")
+                        .withFileFromClasspath("initial-25x.sql", "mysql/initial-25x.sql")
+        ).withExposedPorts(3306).withLogConsumer(new Slf4jLogConsumer(log));
         container.start();
         mysql.setContainer(container);
         return mysql;
@@ -43,19 +36,17 @@ public class Mysql implements Closeable {
     public Properties getConnectionProperties() {
         Properties p = new Properties();
         p.setProperty("connection.username", "root");
-        p.setProperty("connection.password", getEnvironmentVariable("MYSQL_ROOT_PASSWORD"));
-        p.setProperty("connection.url", getContainer().getJdbcUrl());
+        p.setProperty("connection.password", "test");
+        p.setProperty("connection.url", "jdbc:mysql://" + getHost() + ":" + getPort() + "/" + DATABASE_NAME);
         return p;
     }
 
-    public String getEnvironmentVariable(String key) {
-        for (String env : getContainer().getEnv()) {
-            String[] keyValue = env.split("=", 2);
-            if (keyValue[0].trim().equals(key)) {
-                return keyValue.length == 2 ? keyValue[1].trim() : null;
-            }
-        }
-        return null;
+    public String getHost() {
+        return container.getHost();
+    }
+
+    public int getPort() {
+        return container.getMappedPort(3306);
     }
 
     public void close() {
